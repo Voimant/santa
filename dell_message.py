28ec57ce53ee7a1fdedd6526e7645bec7882ab2c
Voimant <voimant007@gmail.com>
2025-11-15 16:32:37 +0500

    fix 3

--- dell_message.py
+++ dell_message.py
@@ -1,24 +1,9 @@
-import asyncio
-import os
-from datetime import time, timedelta
-import datetime
-from difflib import diff_bytes
-from distutils.util import execute
-from sre_parse import parse
-from urllib.request import CacheFTPHandler
 
-from aiogram.enums import ContentType
-from aiohttp.web_routedef import delete
+import os
 
 from DB.db_func import db_message_id
 
 from aiogram import types, Dispatcher, Router, F, Bot
-from aiogram.filters import CommandStart, Command
-from aiogram.types import Message, CallbackQuery
-from aiogram.types import FSInputFile, BufferedInputFile
-from aiogram.fsm.context import FSMContext
-from aiogram.filters.state import State, StatesGroup
-from pydantic.v1.validators import anystr_strip_whitespace
 from aiogram.exceptions import TelegramBadRequest
 
 from dotenv import load_dotenv
